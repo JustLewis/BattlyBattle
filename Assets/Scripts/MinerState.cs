cf55561bb4e289ff9d208e02154bddeb1028e3bc
Lewis <l.marlow@outlook.com>
2020-10-19 11:12:48 +0100

    Initial commit

---
+++ Assets/Scripts/MinerState.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class MinerState <Owner>
+{
+    abstract public void Enter(Owner TheOwner);
+    abstract public void Exit(Owner TheOwner);
+    abstract public void Update(Owner TheOwner); //Shouldn't interfere with Unity's update as it does not inherit from Mono behavior
+}
